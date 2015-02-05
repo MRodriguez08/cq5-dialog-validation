1ca18ea5d128a0fdea19e248d17a0d6a3db0bdf1
unknown <mauricio.rodriguez@glb-d2796.globant.com>
2015-02-05 13:42:31 -0200

    added support for xtype richtext

--- js/cq-dialog-validator.js
+++ js/cq-dialog-validator.js
@@ -1,6 +1,31 @@
 /**
+ * CQ5 Basic Dialog Validator
+ * @author mauricio.rodriguez 
+ */
+
+// support section
+
+/**
+ * helper function to resolve javascript objects types
+ */
+var type = (function(global) {
+    var cache = {};
+    return function(obj) {
+        var key;
+        return obj === null ? 'null' // null
+            : obj === global ? 'global' // window in browser or global in nodejs
+            : (key = typeof obj) !== 'object' ? key // basic: string, boolean, number, undefined, function
+            : obj.nodeType ? 'object' // DOM element
+            : cache[key = ({}).toString.call(obj)] // cached. date, regexp, error, object, array, math
+            || (cache[key] = key.slice(8, -1).toLowerCase()); // get XXXX from [object XXXX], and cache it
+    };
+}(this));
+
+// support section
+
+
+/**
  * Helper class to provide a basic validation for configuration dialogs.
- * @author mauricio.rodriguez
  */
 function CqDialogValidator() {
 	
@@ -11,53 +36,71 @@ function CqDialogValidator() {
 		generalError : "Please check all marked fields",		
 	}
 	
+	/**
+	 * Supported parameter types:
+	 * - string
+	 * - textfield
+	 * - multifield
+	 * - richtext
+	 */
 	this.valueIsEmpty = function(value){
-		return (value.trim().length === 0);
+				
+		var objType = type(value);
+		
+		//add support for extypes
+		if (objType === 'object')
+			objType = value.getXType();
+		
+		if (objType === 'string'){
+			return (value.trim().length === 0);
+		} else if (objType === 'textfield' || objType === 'richtext'){
+			return (value.getValue().trim().length === 0);
+		} else if (objType === 'multifield'){
+			return (value.getValue().length === 0);
+		} else
+			return false;
 	}
 	
 	/**
-	 * Validates all dialog fields marked as required in dialog.xml
+	 * validates all dialog fields marked as required in dialog.xml
 	 * cls="required".
-	 * Supported field types:
+	 * supported field types:
 	 * - multifield
 	 * - textfield
+	 * - richtext
 	 */
 	this.validateDialog = function(dialog){
-		console.log("config dialog validation...");
 		var valid = true;
 		var error = "";
 		
-		//check required multifields
 		var i = 0;
-		// retrieve all fields without considering the type
+		//we use box as type parameter to select all fields no matter the type.
+		//findByType( String/Class xtype, [Boolean shallow] ) : Array
+		//note that by default shallow = false, so funtion findByType will search for 
+		//subclasses of box to...
 		var multifields = dialog.findByType("box");		
 		while(i < multifields.length){
 			var iterationValid = true;
 			var field = multifields[i];
-			if (field.hasClass("required")){			
-				if (field.getXType() == 'multifield'){				
-					if (field.getValue().length == 0){					
-						valid = false;
-						iterationValid = false;
-					}
-				} else if (field.getXType() == 'textfield'){
-					if (this.valueIsEmpty(field.getValue())){
-						valid = false;
-						iterationValid = false;
-					}			
+			if (field.hasClass("required")){
+				if (this.valueIsEmpty(field)){
+					valid = false;
+					iterationValid = false;
 				}
+				
+				//update field state
 				if (iterationValid){				
 					this._markValid(field);
 				} else {
 					this._markInvalid(field);
-				}
-			}			
+				}	
+			}
 			i++;
 		}
 		
 		if (!valid){
 			CQ.Ext.Msg.show({
-				title:'Validation Error',
+				title: 'Validation Error',
 				msg: this.errorMessages["generalError"],
 				buttons:CQ.Ext.MessageBox.OK,
 				icon:CQ.Ext.MessageBox.ERROR}
@@ -76,33 +119,21 @@ function CqDialogValidator() {
 		while(i < multifields.length){
 			var iterationValid = true;
 			var field = multifields[i];
-			if (field.hasClass("required")){					
-				if (field.getXType() == 'multifield') { //if the field is multifield
-					if (field.getValue().length == 0){
-						iterationValid = false;
-					}
-				} else if (field.getXType() == 'textfield'){ //if the field is textfield
-					if (this.valueIsEmpty(field.getValue())){
-						iterationValid = false;
-					}			
+			if (field.hasClass("required")){
+				if (this.valueIsEmpty(field)){
+					valid = false;
+					iterationValid = false;
 				}
+				
+				//update field state
 				if (iterationValid){				
 					this._markValid(field);
 				} else {
 					this._markInvalid(field);
 				}
-			}			
+			}
 			i++;
 		}
-	}
-	
-	/**
-	 * Creates the key for the messages array
-	 */
-	this._getFieldMessageKey = function(fieldName){
-		//we remove the "./"
-		var key = fieldName.substring(2, fieldName.length);
-		return key;
 	}
 	
 	/**
@@ -113,9 +144,8 @@ function CqDialogValidator() {
 		if (field.getXType() == 'multifield'){
 			//manually add css clases to update field status
 			//mutifield "invalid" state is not correctly shown :s
-			field.addClass("x-form-invalid");
-			field.addClass("x-panel");
-		} else if (field.getXType() == 'textfield'){
+			field.addClass("x-panel x-form-invalid");
+		} else if (field.getXType() == 'textfield' || field.getXType() == 'richtext'){
 			field.markInvalid();
 		}
 	}
